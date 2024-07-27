9187419cc4d47f877209e6d43f4970509e04057a
admin <admin@DESKTOP-T4JI2A7>
2024-07-27 22:53:08 +0800

    开发者2新建分支

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,8 @@
             Console.WriteLine("测试1");
             Console.WriteLine("开发者2提交测试2");
             Console.WriteLine("开发者1提交测试3");
+
+            Console.WriteLine("开发者2新建分支");
         }
     }
 }
