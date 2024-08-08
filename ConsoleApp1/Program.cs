ed7b77628b8e062406e38360ad5d308d55e61255
dmibing <45461531+dmibing@users.noreply.github.com>
2024-08-08 15:14:28 +0800

    1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            //
             Console.WriteLine("Hello, World!");
             Console.WriteLine("测试1");
             Console.WriteLine("开发者2提交测试2");
