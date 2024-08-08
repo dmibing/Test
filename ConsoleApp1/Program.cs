b89802c4502a21de0056d1a415f0983d9b0dd502
dmibing <45461531+dmibing@users.noreply.github.com>
2024-08-08 16:23:09 +0800

    1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,17 +1,12 @@
-﻿namespace ConsoleApp1
+﻿using System.Linq.Expressions;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("测试1");
-            Console.WriteLine("开发者2提交测试2");
-            Console.WriteLine("开发者1提交测试3");
-
-            Console.WriteLine("开发者2新建分支");
-            Console.WriteLine("开发者1修改测试3");
+          
         }
     }
 }
